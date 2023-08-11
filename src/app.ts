57b1dd9bcc278d4ee1d771eeb201877751cc30b5
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-11 16:07:22 -0300

    Feat: Utiliza a rota order

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import productRouter from './routes/product.route';
+import orderRouter from './routes/order.route';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', productRouter);
+app.use('/orders', orderRouter);
 
 export default app;
