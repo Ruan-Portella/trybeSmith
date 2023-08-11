1fe135cf6744ef2aca77c8f745da00b6b304cb64
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-11 17:02:17 -0300

    Feat: Coloca validações na rota de criar produto

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import productController from '../controllers/product.controller';
+import nameValidation from '../middlewares/name.validations';
+import priceValidation from '../middlewares/price.validations';
 
 const productRouter = express.Router();
 
-productRouter.post('/', productController.createProduct);
+productRouter.post('/', nameValidation, priceValidation, productController.createProduct);
 productRouter.get('/', productController.getAllProducts);
 
 export default productRouter;
