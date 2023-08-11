4fee2ab851e8fdf87cd21b637612b9f60bc66f1d
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-11 18:13:16 -0300

    Feat: Cria test de integração para listar todas orders

--- tests/integration/orders/list.test.ts
+++ tests/integration/orders/list.test.ts
@@ -1,10 +1,19 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app'
+import OrderModel from '../../../src/database/models/order.model';
+import ProductModel from '../../../src/database/models/product.model';
 
 chai.use(chaiHttp);
 
 describe('GET /orders', function () { 
   beforeEach(function () { sinon.restore(); });
+  it('Testa se é possível criar um novo pedido', async function () {
 
+    sinon.stub(OrderModel, 'findAll').resolves([]);
+    const Response = await chai.request(app).get('/orders').send()
+    
+    expect(Response.status).to.equal(200);
+  });
 });
