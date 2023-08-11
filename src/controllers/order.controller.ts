6468e6deaaf00d447ef7a4b6da305313020da918
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-11 18:12:10 -0300

    Feat: Cria função para criar uma nova order no controller

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -6,6 +6,13 @@ const getAllOrders = async (req: Request, res: Response) => {
   res.status(200).json(orders);
 };
 
+const createOrder = async (req: Request, res: Response) => {
+  const { userId, productIds } = req.body;
+  const order = await orderService.createOrder(userId, productIds);
+  res.status(201).json(order);
+};
+
 export default {
   getAllOrders,
+  createOrder,
 };
