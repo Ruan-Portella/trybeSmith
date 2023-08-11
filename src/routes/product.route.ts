995e4c10c86cd3e44d3dbb57c77cd2ae2759d15f
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-11 15:16:20 -0300

    Feat: Cria rota get para pegar todos os produtos

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -4,5 +4,6 @@ import productController from '../controllers/product.controller';
 const productRouter = express.Router();
 
 productRouter.post('/', productController.createProduct);
+productRouter.get('/', productController.getAllProducts);
 
 export default productRouter;
