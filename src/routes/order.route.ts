310dc1d82e1c34565a1470bdcebaf634cf16400f
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-11 18:12:56 -0300

    Feat: Cria rota post para orders

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,8 +1,18 @@
 import express from 'express';
 import orderController from '../controllers/order.controller';
+import authValidation from '../middlewares/auth.validations';
+import userValidation from '../middlewares/user.validations';
+import productValidation from '../middlewares/product.validations';
 
 const orderRouter = express.Router();
 
 orderRouter.get('/', orderController.getAllOrders);
+orderRouter.post(
+  '/', 
+  authValidation, 
+  userValidation,
+  productValidation, 
+  orderController.createOrder,
+);
 
 export default orderRouter;
