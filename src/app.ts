6a907f9b1c07f690d6c44dc6c9810ccd5d98c8ce
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-11 16:51:21 -0300

    Feat: Utiliza rota login

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import productRouter from './routes/product.route';
 import orderRouter from './routes/order.route';
+import loginRouter from './routes/login.route';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', productRouter);
 app.use('/orders', orderRouter);
+app.use('/login', loginRouter);
 
 export default app;
