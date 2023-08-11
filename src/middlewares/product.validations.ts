b71ad017b9dc01a6ba8cc7b9c2bd96d21b95ca72
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-11 18:12:33 -0300

    Feat: Cria validação de produto

---
+++ src/middlewares/product.validations.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express';
+
+const productValidation = (req: Request, res: Response, next: NextFunction) => {
+  const { productIds } = req.body;
+  if (!productIds) {
+    return res.status(400).json({ message: '"productIds" is required' });
+  }
+  if (!Array.isArray(productIds)) {
+    return res.status(422).json({ message: '"productIds" must be an array' });
+  }
+  if (productIds.length === 0) { 
+    return res.status(422).json({ message: '"productIds" must include only numbers' });
+  }
+  
+  next();
+};
+
+export default productValidation;
